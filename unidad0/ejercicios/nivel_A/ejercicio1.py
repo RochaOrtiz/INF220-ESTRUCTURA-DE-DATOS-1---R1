c047cc73de4064eb7c0ab80b18afa6cd1d968b81
MariaReneRocha <maneux10@gmail.com>
2026-08-26 20:46:09 -0400

    Refactor calcularpromedio function to calcular_promedio
    Refactored code to follow PEP8 style guidelines, including function and variable naming conventions.

--- unidad0/ejercicios/nivel_A/ejercicio1.py
+++ unidad0/ejercicios/nivel_A/ejercicio1.py
@@ -1,1 +1,33 @@
+#PRIMER CODIGO SIN PEP8
+def calcularpromedio(lista):
+    suma=0
+    for i in lista:
+        suma=suma+i
+    return suma/len(lista)
 
+nombre="Carlos"
+edAD=20
+notas=[80,90,75,85]
+
+print("Nombre:",nombre)
+print("Edad:",edAD)
+print("Promedio:",calcularpromedio(notas))
+
+#CODIGO CORREGIDO USANDO LA PRACTICA DE PEP8
+
+def calcular_promedio(notas):
+    suma = 0
+
+    for nota in notas:
+        suma = suma + nota
+
+    return suma / len(notas)
+
+
+nombre = "Carlos"
+edad = 20
+notas = [80, 90, 75, 85]
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Promedio:", calcular_promedio(notas))
